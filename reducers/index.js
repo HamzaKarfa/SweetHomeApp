4f407dbf629f99fd42b3dddb471789322ac5f59b
hamza karfa <hamza.karfa@gmail.com>
2022-02-06 19:52:59 +0100

    setup redux + redux-thunk

---
+++ reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+
+};
+  
+  function rootReducer(state = initialState, action) {
+    let newState;
+    switch (action.type) {
+      default:
+        return state;
+    }
+  }
+  
+  export default rootReducer;
